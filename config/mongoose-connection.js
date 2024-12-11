7243a7aa804c2ec565430cbd1eaddf6e86d43124
Anil Kumar <141206886+Anilkumar7877@users.noreply.github.com>
2024-12-11 22:43:49 +0530

    Update mongoose-connection.js

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 mongoose
-.connect('mongodb+srv://anilpython7877:GZ6t7pp2aDaXxEoG@cluster0.2gx7j.mongodb.net/scratch_ecommerce?retryWrites=true&w=majority&appName=Cluster0')
+.connect('your_mongodb_database_uri')
 .then(function(){
     console.log("MongoDB Connected.");
 })
